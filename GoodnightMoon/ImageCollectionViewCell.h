ea92c433737ac6d5fa8e7922c62434249f2f75d3
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-15 11:57:12 -0600

    initial commit

---
+++ GoodnightMoon/ImageCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.h
+//  GoodnightMoon
+//
+//  Created by Syed Amaanullah on 1/15/15.
+//  Copyright (c) 2015 Syed Amaanullah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCollectionViewCell : UICollectionViewCell
+@property IBOutlet UIImageView *imageView;
+
+@end
---
+++ GoodnightMoon/ImageCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.m
+//  GoodnightMoon
+//
+//  Created by Syed Amaanullah on 1/15/15.
+//  Copyright (c) 2015 Syed Amaanullah. All rights reserved.
+//
+
+#import "ImageCollectionViewCell.h"
+
+@implementation ImageCollectionViewCell
+
+@end
